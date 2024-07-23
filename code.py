8c6b63aefade7991f7ec565200cd23bd97927be2
sam blenny <68084116+samblenny@users.noreply.github.com>
2024-07-23 21:39:45 +0000

    improve scan and nav menu

--- code.py
+++ code.py
@@ -3,10 +3,14 @@
 #
 # ble-gamepad
 #
-# Docs:
+# Docs and Reference Code:
 # - https://learn.adafruit.com/adafruit-i2c-qt-rotary-encoder/python-circuitpython
 # - https://docs.circuitpython.org/projects/seesaw/en/latest/api.html
 # - https://docs.circuitpython.org/projects/ble/en/latest/api.html
+# - https://github.com/adafruit/circuitpython/blob/main/shared-bindings/_bleio/ScanResults.c
+# - https://github.com/adafruit/circuitpython/blob/main/shared-bindings/_bleio/ScanEntry.c
+# - https://github.com/adafruit/Adafruit_CircuitPython_BLE/blob/main/adafruit_ble/__init__.py
+# - https://www.bluetooth.com/specifications/assigned-numbers/
 #
 from board import STEMMA_I2C
 from gc import collect, mem_free
@@ -24,7 +28,7 @@ def gcCol():
     print('mem_free', mem_free())
 
 def drainCDCBuf():
-    # Drain the serial console buffer.
+    # Drain the serial console buffer
     cons = usb_cdc.console
     if cons:
         w = cons.in_waiting
@@ -32,34 +36,71 @@ def drainCDCBuf():
             cons.timeout = 0
             cons.read(w)
 
-def showMenu(choices, selection):
+def showMenu(choices, context):
     # Show the menu with selected item highlighted
-    assert (0 <= selection) and (selection < len(choices)), 'selection OOR'
+    assert (type(context) == dict) and ('selection' in context), "bad context"
+    selection = context['selection']
+    assert (0 <= selection) and (selection < len(choices)), "bad selection"
+    print("Menu: ", end='')
     for (i, (name, _)) in enumerate(choices):
         if i == selection:
-            print('[[%s]] ' % name, end='')
+            print('>>%s<<' % name, end='')
         else:
-            print('  %s   ' % name, end='')
+            print('  %s  ' % name, end='')
     print()
 
-def doMenuAction(choices, selection):
+def doMenuAction(choices, context):
     # Perform the action for the selected menu item
-    assert (0 <= selection) and (selection < len(choices)), 'selection OOR'
+    assert (type(context) == dict) and ('selection' in context), "bad context"
+    selection = context['selection']
+    assert (0 <= selection) and (selection < len(choices)), "bad selection"
     (name, actionFn) = choices[selection]
     if actionFn is None:
         print(name, 'is not implemented yet')
     else:
-        actionFn()
+        actionFn(context)
 
-def scan():
-    # Scan for BLE devices
-    t = 30
+def scan(context):
+    # Scan for BLE devices, scan results get saved to context['devices']
+    assert (type(context) == dict) and ('devices' in context), "bad context"
+    assert (type(context['devices']) == dict), "bad device dictionary"
+    t = 15
     print('Scanning with %d second timeout' % t)
     radio = BLERadio()
-    for adv in radio.start_scan(timeout=t):
-        print(adv)
-    print('Scan done')
+    devices = {}
+    for r in radio.start_scan(timeout=t):
+        name = r.complete_name
+        if (not r.connectable) or (name is None):
+            # Ignore advertisements that are unconnectable or unnamed
+            continue
+        key = (r.address, name)
+        if key in devices:
+            # Ignore advertisements we've already seen
+            continue
+        # Handle a new advertisement
+        devices[key] = r
+        print(key)
+    context['devices'] = devices
+    print('Scan done\n')
 
+def devices(context):
+    # Print the list of devices from the most recent scan
+    assert (type(context) == dict) and ('devices' in context), "bad context"
+    assert (type(context['devices']) == dict), "bad device dictionary"
+    print('Connectable BLE devices:')
+    if len(context['devices']) == 0:
+        print('[no devices, try scanning again]')
+    for k in context['devices']:
+        print(k)
+    print()
+
+def connect(context):
+    # Connect to a BLE device
+    print('Connect is not implemented yet\n')
+
+def disconnect(context):
+    # Disconnect from a BLE device
+    print('Disconnect is not implemented yet\n')
 
 def main():
     # Initialize stuff then start event loop
@@ -70,17 +111,19 @@ def main():
     sswver = (ssw.get_version() >> 16) & 0xffff
     assert (sswver == 4991), 'unexpected seesaw firmware version (not 4991)'
     ssw.pin_mode(24, Seesaw.INPUT_PULLUP)    # add pullup to knob-click button
+    # NAV MENU
+    # each item should be a tuple of (name, bound-function)
+    choices = [
+        ('Scan', scan),
+        ('Devices', devices),
+        ('Connect', connect),
+        ('Disconnect', disconnect),
+    ]
+    # GLOBAL STATE
+    context = {'devices': {}, 'selection': 0}
     # MAIN EVENT LOOP
     prevClick = False
-    choices = [         # each item should be a tuple of (name, bound-function)
-        ('Scan', scan),
-        ('Pair', None),
-        ('Info', None),
-        ('Connect', None),
-        ('Disconnect', None),
-    ]
-    selection = 0
-    showMenu(choices, selection)
+    showMenu(choices, context)
     while True:
         sleep(0.01)
         # Check for newline wake sequence on the serial console
@@ -88,16 +131,17 @@ def main():
         if cons and cons.in_waiting > 0:
             # for any console input (probably LF) -> send menu
             drainCDCBuf()
-            showMenu(choices, selection)
+            showMenu(choices, context)
         # Read the rotary encoder (Seesaw I2C)
         (click, delta) = (not ssw.digital_read(24), ssw.encoder_delta())
         if click and (click != prevClick):
             # knob was clicked
-            doMenuAction(choices, selection)
+            doMenuAction(choices, context)
+            showMenu(choices, context)
         prevClick = click
         if delta != 0:
             # knob was turned
-            selection = (selection + delta) % len(choices)
-            showMenu(choices, selection)
+            context['selection'] = (context['selection'] + delta) % len(choices)
+            showMenu(choices, context)
 
 main()
